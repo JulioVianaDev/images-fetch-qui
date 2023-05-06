74f7b63f22771eae532cc7d449f13d931630190b
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-06 11:24:39 -0300

    mostrando a imagem

--- styles.js
+++ styles.js
@@ -1,28 +1,36 @@
 const body = document.getElementById("body");
   const icons = document.getElementById("temasOptions");
 
-  function toggleTheme() {
-    const moonIcon = '<img id="moon" src="./images/moon.png" onclick="toggleTheme()"/>';
-    const sunIcon = '<img id="sun" src="./images/sun.png" onclick="toggleTheme()"/>';
+function toggleTheme() {
+  const moonIcon = '<img id="moon" src="./images/moon.png" onclick="toggleTheme()"/>';
+  const sunIcon = '<img id="sun" src="./images/sun.png" onclick="toggleTheme()"/>';
 
-    if (body.classList.contains("body-dark")) {
-      body.classList.remove("body-dark");
-      body.classList.add("body-light");
-      icons.innerHTML = moonIcon;
-    } else {
-      body.classList.remove("body-light");
-      body.classList.add("body-dark");
-      icons.innerHTML = sunIcon;
-    }
+  if (body.classList.contains("body-dark")) {
+    body.classList.remove("body-dark");
+    body.classList.add("body-light");
+    icons.innerHTML = moonIcon;
+  } else {
+    body.classList.remove("body-light");
+    body.classList.add("body-dark");
+    icons.innerHTML = sunIcon;
   }
-  const fileInput = document.getElementById("myFileInput");
+}
+const fileInput = document.getElementById("myFileInput");
+const imageLabel = document.getElementById("imageLabel");
 
-  fileInput.addEventListener("change", function() {
-    if (fileInput.files.length > 0 && fileInput.files[0].type.startsWith("image/")) {
-      // A file has been selected and it's an image
-      console.log("An image has been selected");
-    } else {
-      // No file selected or the selected file is not an image
-      console.log("No image selected");
+fileInput.addEventListener("change", function() {
+  if (fileInput.files.length > 0 && fileInput.files[0].type.startsWith("image/")) {
+    // A file has been selected and it's an image
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+      // Set the background-image of the imageLabel to the selected image
+      imageLabel.style.backgroundImage = `url(${event.target.result})`;
     }
-  });
+
+    reader.readAsDataURL(fileInput.files[0]);
+  } else {
+    // No file selected or the selected file is not an image
+    imageLabel.style.backgroundImage = "";
+  }
+});
